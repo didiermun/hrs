a8dd51304cb453c914c0091e9bf6462d6051afb6
didiermun <didiermunezer38@gmail.com>
2022-02-04 17:50:19 +0200

    fix: search page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,7 @@
-import Header from "~/components/Header";
-
 export default function Index() {
   return (
     <div className="min-h-screen">
-     <Header></Header>
+     Home page
     </div>
   );
 }
---
+++ app/routes/search.tsx
@@ -0,0 +1,9 @@
+import Header from "~/components/Header";
+
+export default function Search() {
+  return (
+    <div className="min-h-screen">
+     <Header></Header>
+    </div>
+  );
+}
