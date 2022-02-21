4d1d775cf47fc12295842def6a04870dc6f3f205
didiermun <didiermunezer38@gmail.com>
2022-02-21 12:09:07 +0200

    feat: project structure!

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+    return(
+        <div className="bg-white flex flex-col mx-auto item-center justify-center">
+            <p className="text-5xl font-semibold text-black">Page not found</p>
+        </div>
+    )
+}
