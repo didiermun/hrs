e5c1db6ad5a51c90a2d7b9d65843f513c446af2a
didiermun <didiermunezer38@gmail.com>
2022-02-27 02:11:14 +0200

    feat: add nice looking 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom';
+import notfound from '../files/404.svg';
 export default function NotFound() {
     return(
-        <div className="h-screen w-screen bg-white flex flex-col mx-auto item-center justify-center">
-            <div className="w-full">
-                <h3 className="text-center text-5xl font-bold">404</h3>
-                <p className="text-3xl font-semibold text-black text-center">Page not found</p>
-            </div>
+        <div className="h-screen w-screen bg-white flex flex-col mx-auto items-center justify-center gap-20">
+                <div className="flex w-max">
+                    <img src={notfound} alt="" />
+                </div>
+                <div className='flex flex-col w-max items-center gap-10'>
+                    <p className='text-[#565872] font-semibold text-5xl'>OOPS! PAGE NOT FOUND</p>
+                    <Link to="/">
+                    <button className='rounded-xl px-6 py-3 bg-secondary w-max text-white text-medium'>BACK TO HOME</button>
+                    </Link>
+                </div>
         </div>
     )
 }
