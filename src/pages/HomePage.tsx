ae2ba3463a5a9abab97b06f17ca3f67a629c23f2
didiermun <didiermunezer38@gmail.com>
2022-03-04 03:37:04 +0200

    fix: links

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Atom/Button";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -7,13 +8,13 @@ import Navbar from "../components/Navbar";
 
 
 const  HomePage = () => {
-    const [count, setCount] = useState(0);
+    const navigator = useNavigate();
     return (
     <div className="bg-dark">
         <Navbar/>
         <div className="bg-white px-6 md:px-20 min-h-screen flex flex-col items-center mx-auto justify-center">
-            <p>Count is {count}</p>
-            <Button className="w-80" onClick={()=>{setCount(count+4)}}>Update count</Button>
+            <p>Here we go!</p>
+            <Button className="w-80" onClick={()=>{navigator('/auth/signin')}}>Signin</Button>
         </div>
         <Footer/>
     </div>
